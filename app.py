a56d6ee8f5733ac18d69224a1f7ed74e94b51f2c
Ashish Lama <lama.ashish07@gmail.com>
2024-09-30 01:58:53 -0400

    Adds pkl model file for decision tree and adaboost

--- app.py
+++ app.py
@@ -8,7 +8,9 @@ import numpy as np
 
 app = Flask(__name__)
 
-model = pickle.load(open('model.pkl', 'rb'))
+dt_model = pickle.load(open('DT_model.pkl', 'rb'))
+adab_model = pickle.load(open('Adaboost-model.pkl', 'rb'))
+# rf_model = pickle.load(open('RandomForest-model', 'rb'))
 
 # Route for the Home Page (UI)
 @app.route('/')
@@ -18,8 +20,51 @@ def home():
 # Route for Predicting and Showing LIME/SHAP Results
 @app.route('/predict', methods=['POST'])
 def predict():
+    model_selected = request.form['model']
+    if model_selected == 'model1':
+        model = dt_model
+        feature_names = ['review_count_x'
+                        ,'avg_review_stars'              
+                        ,'review_count_per_bussiness'    
+                        ,'checkin_count'                 
+                        ,'tip_count'                     
+                        ,'total_compliments'             
+                        ,'RestaurantsDelivery'           
+                        ,'OutdoorSeating'                
+                        ,'BusinessAcceptsCreditCards'    
+                        ,'BikeParking'                   
+                        ,'WiFi'                          
+                        ,'Caters'                        
+                        ,'WheelchairAccessible'          
+                        ,'BusinessParking'               
+                        ,'RestaurantsPriceRange2'        
+                        ,'weekly_hours']
+        
+    # elif model_selected == 'model2':
+    #     model = rf_model
+    
+    elif model_selected == 'model3':
+        model = adab_model
+        feature_names = ['review_count_x'
+                        ,'avg_review_stars'              
+                        ,'review_count_per_bussiness'
+                        ,'avg_fans_per_reviewer'    
+                        ,'checkin_count'                 
+                        ,'tip_count'                     
+                        ,'total_compliments'             
+                        ,'RestaurantsDelivery'           
+                        ,'OutdoorSeating'                
+                        ,'BusinessAcceptsCreditCards'    
+                        ,'BikeParking'
+                        ,'RestaurantsTakeOut'                   
+                        ,'WiFi'                          
+                        ,'Caters'                        
+                        ,'WheelchairAccessible'          
+                        ,'BusinessParking'               
+                        ,'RestaurantsPriceRange2'        
+                        ,'weekly_hours']
     # Get data from the form
-    data = [float(request.form[feature]) for feature in feature_names]
+    data = [float(request.form[feature]) if request.form[feature] != '' else 0.0 for feature in feature_names]
     
     # Convert input into DataFrame
     input_data = pd.DataFrame([data], columns=feature_names)
@@ -29,26 +74,6 @@ def predict():
 
     # Return result with LIME and SHAP visualizations
     return render_template('result.html', prediction=int(prediction))
-
-# @app.route('/predict', methods=['POST'])
-# def predict():
-#     data = request.get_json(force=True)
-
-#     input_data = pd.DataFrame([data], columns=feature_names)
-
-#     prediction = model.predict(input_data)[0]
-
-#     # lime_exp = explainer.explain_instance(input_data.values[0], model.predict_proba, num_features=2)
-#     # lime_html = lime_exp.as_html()
-
-#     # shap_values = shap_explainer.shap_values(input_data)
-#     # shap_summary = shap.force_plot(shap_explainer.expected_value[1], shap_values[1], input_data)
-
-#     return jsonify({
-#         'prediction': int(prediction),
-#         'lime_interpretation': lime_html,
-#         'shap_interpretation': str(shap_summary)
-#     })
 
 if __name__ == '__main__':
     app.run(debug=True)
